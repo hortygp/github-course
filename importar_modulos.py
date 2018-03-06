8266ed69d23caebdb7647cc0f838ada9b499d6d1
isabelBarros <hortygp@gmail.com>
2018-03-06 10:13:54 -0300

    sobre modulos

---
+++ importar_modulos.py
@@ -0,0 +1,6 @@
+import math #importou a biblioteca math
+e = math.e #usamos o ponto ai ele vai procurar a constante de euler
+pi = math.pi
+
+print(e) #vai mostrar a constante de euler
+print(pi) #vai mostrar a constante de pi
