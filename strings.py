8c6d34f8bece633f9662003c7efe0fa139aac702
isabelBarros <hortygp@gmail.com>
2018-03-06 10:10:14 -0300

    sobre strings

---
+++ strings.py
@@ -0,0 +1,25 @@
+#s1= 'aaa'
+#print(s1)
+
+#s="para voce meu amor"
+#print (s[0])
+#print (s[5:10]) # cortar de 5 até 10
+#print (s[: :-1]) de tras pra frente
+#print (s[: : 5]) passo de 5 em 5
+
+#exemplo1
+#s='iterando strings'
+#for c in s:
+ #   print (c)
+
+#exemplo 2
+#s='iterando strings'
+#indice = 0
+
+#while indice < len(s):
+ #   print(indice, s[indice])
+  #  indice+=1
+
+#exemplo 3
+for k, v in enumerate('iterando strings'):
+    print(k, v)
