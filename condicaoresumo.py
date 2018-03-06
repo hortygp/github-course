c676fe031feb933a310e33118f577dddd55a7a45
isabelBarros <hortygp@gmail.com>
2018-03-06 09:47:07 -0300

    versao condicoes

---
+++ condicaoresumo.py
@@ -0,0 +1,23 @@
+numero1 = input("digite um numero: ")
+numero1 = int(numero1)
+
+numero2 = input("digite um numero: ")
+numero2 = int(numero2)
+
+if (numero1 == numero2):#operador d eigualdade
+    print("O numero %d é igual a %d." %(numero1, numero2))
+
+if (numero1 != numero2): #operador de diferença
+        print("O numero %d é diferente de %d." % (numero1, numero2))
+
+if (numero1 < numero2): #operador menor que
+        print("O numero %d é menor que %d." % (numero1, numero2))
+
+if (numero1 > numero2): #operador maior que
+        print("O numero %d é maior que %d." % (numero1, numero2))
+
+if (numero1 >= numero2): #operador maior ou igual que
+        print("O numero %d é maior ou igual que %d." % (numero1, numero2))
+
+if (numero1 <= numero2): #operador menor ou igual que
+        print("O numero %d é menor ou igual que %d." % (numero1, numero2))
